187b332f104db78156291ace43bb530f03700c1e
Arran Maclean <Arran.Maclean@defra.gov.uk>
2022-01-22 09:37:29 +0000

    re-added click me button

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,3 +2,10 @@ const runMe = (function () {
     let myTarget: HTMLElement = document.querySelector('#hello-test')!;
     return myTarget.innerHTML = `Hello test`;
 }());
+
+const listenForButtonClick = (function () {
+    let myButton: HTMLElement = document.querySelector('#click-me')!;
+    myButton.addEventListener('click', function () {
+        alert('You clicked me!');
+    });
+}());
